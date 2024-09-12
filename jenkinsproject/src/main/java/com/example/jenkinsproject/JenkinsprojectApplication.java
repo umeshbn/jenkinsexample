0dc3ab785863fc0c2c4115faec2bf5e3f70243c3
umeshbn <96108616+umeshbn@users.noreply.github.com>
2024-09-13 00:06:17 +0530

    first commit

---
+++ jenkinsproject/src/main/java/com/example/jenkinsproject/JenkinsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsprojectApplication.class, args);
+	}
+
+}
